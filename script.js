a16077e7bd0c41d73aa9671b09fab6b22fad8c61
Kumarabhi9094 <147074910+Kumarabhi9094@users.noreply.github.com>
2023-11-30 23:59:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function performSearch() {
+    // You can add your search logic here
+    var searchQuery = document.getElementById('searchInput').value;
+    alert('Searching for: ' + searchQuery);
+    // Add more advanced search functionality as needed
+}
